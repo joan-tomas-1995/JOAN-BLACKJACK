f13a98d04fa04b42a7ca47825bc81dbd5f7ee6cd
joan-tomas-1995 <joan.tomas.miralles@gmail.com>
2023-03-18 09:28:25 +0100

    Actualizado a 18/03/2023

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-
 /* DECLARACION DE VARIABLES */
 
 let botonStart = document.querySelector(".btn-start");
 let botonPlantarse = document.querySelector(".btn-plantarse");
 let botonRepartir = document.querySelector(".btn-repartir");
-let botonReset = document.querySelector(".btn-reset")
+let botonReset = document.querySelector(".btn-reset");
 let mostrarCartas = document.querySelector(".mostrar-carta");
 let puntosJugador = document.querySelector(".puntos-jugador");
-
+let mensajeJugador = document.querySelector(".mensaje");
 
 /* ESCONDER START BUTTON */
 
 botonPlantarse.style.visibility = "hidden";
 botonRepartir.style.visibility = "hidden";
 botonReset.style.visibility = "hidden";
-
 
 function hideButton() {
   botonStart.style.visibility = "hidden";
@@ -28,13 +26,20 @@ botonStart.addEventListener("click", hideButton);
 
 /*  BOTON PARA REPARTIR CARTA */
 
-const palosCartas = ["♠", "♣", "♥", "♦"];
+let pica = "♠";
+let trebol = "♣";
+let corazon = "♥";
+let rombo = "♦";
+
+const palosCartas = [pica, trebol, corazon, rombo];
 const numCartas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 botonRepartir.addEventListener("click", () => {
   const [cartaRandom, puntosCarta] = obtenerCartaAleatoria();
   arrayCartasJugador.push(cartaRandom);
   sumaPuntosJugador.push(puntosCarta);
   mostrarCartasFuncion();
+    verificarPuntos();
+
 });
 
 function obtenerCartaAleatoria() {
@@ -42,7 +47,7 @@ function obtenerCartaAleatoria() {
     palosCartas[Math.floor(Math.random() * palosCartas.length)];
   const numAleatorio = numCartas[Math.floor(Math.random() * numCartas.length)];
   const cartaRandom = numAleatorio + paloAleatorio;
-  const puntosCarta = numAleatorio <= 10 ? numAleatorio : 10; // Consideramos las figuras como valen 10 puntos
+  const puntosCarta = numAleatorio <= 10 ? numAleatorio : 10;
   return [cartaRandom, puntosCarta];
 }
 
@@ -50,7 +55,31 @@ const arrayCartasJugador = [];
 const sumaPuntosJugador = [];
 
 function mostrarCartasFuncion() {
-  mostrarCartas.innerText = "Cartas: " + arrayCartasJugador.join(", ");
+  mostrarCartas.innerText = `Cartas: ${arrayCartasJugador.join(", ")}`;
   const puntosTotales = sumaPuntosJugador.reduce((a, b) => a + b, 0);
-  puntosJugador.innerText = "Puntos: " + puntosTotales;
+  puntosJugador.innerText = `Puntos: ${puntosTotales}`;
 }
+
+/* RECARGAR PAGINA (RESET) */
+
+function resetPagina() {
+  location.reload();
+}
+
+const btnReset = () => resetPagina();
+botonReset.addEventListener("click", resetPagina);
+
+function verificarPuntos() {
+  const puntosTotales = sumaPuntosJugador.reduce((a, b) => a + b, 0);
+  if (puntosTotales > 21) {
+    mensajeJugador.innerText = "¡Has perdido!";
+    botonPlantarse.style.visibility = "hidden";
+    botonRepartir.style.visibility = "hidden";
+  }
+    if (puntosTotales === 21) {
+      mensajeJugador.innerText = "¡Has ganado!";
+      botonPlantarse.style.visibility = "hidden";
+      botonRepartir.style.visibility = "hidden";
+    }
+}
+
